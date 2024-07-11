ffaff1aded218c9427ff4f3f04c13c62506916f4
Carlos Acosta <carlosaac23@hotmail.com>
2024-07-11 18:29:15 -0500

    Changed button hover

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -9,9 +9,7 @@ const Contact: React.FC = () => {
   });
   const [message, setMessage] = useState('');
 
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
@@ -21,12 +19,7 @@ const Contact: React.FC = () => {
 
     // serviceID - templateID - #form - publicKey
     emailjs
-      .sendForm(
-        'service_vzkaygi',
-        'template_f3xf32h',
-        '#contacto-form',
-        'vWS_GMkpEirykpGRp'
-      )
+      .sendForm('service_vzkaygi', 'template_f3xf32h', '#contacto-form', 'vWS_GMkpEirykpGRp')
       .then(() => {
         // Show sent message
         setMessage('Mensaje enviado exitosamente ✅');
@@ -53,19 +46,14 @@ const Contact: React.FC = () => {
     <section className='contact section' id='contact'>
       <h2 className='text-2xl geist-bold text-[#d4d4d4] mb-10'>Contact me</h2>
       <div className='contact-container container grid'>
-        <form
-          action=''
-          className='contact-form grid gap-4'
-          id='contacto-form'
-          onSubmit={handleSubmit}
-        >
+        <form action='' className='contact-form grid gap-4' id='contacto-form' onSubmit={handleSubmit}>
           <div className='contact-group grid gap-4'>
             <input
               type='text'
               name='userName'
               required
               placeholder='Introduce your name'
-              className='contact-input p-4 rounded-lg border-2 border-[#0e0e0e] bg-[#0A0A0A] placeholder-[#A3A3A3] text-white geist-regular focus:outline-none focus:border-[#27272a] transition-colors duration-300 hover:border-1 hover:border-[#27272a]'
+              className='contact-input p-4 rounded-lg border-2 border-[#0e0e0e] bg-[#0A0A0A] placeholder-[#A3A3A3] text-white geist-regular focus:outline-none focus:border-[#27272a] transition-colors duration-500 hover:border-1 hover:border-[#27272a]'
               value={formData.userName}
               onChange={handleChange}
             />
@@ -74,7 +62,7 @@ const Contact: React.FC = () => {
               name='userEmail'
               required
               placeholder='Introduce your email'
-              className='contact-input p-4 rounded-lg border-2 border-[#0e0e0e] bg-[#0A0A0A] placeholder-[#A3A3A3] text-white geist-regular focus:outline-none focus:border-[#27272a] transition-colors duration-300 hover:border-1 hover:border-[#27272a]'
+              className='contact-input p-4 rounded-lg border-2 border-[#0e0e0e] bg-[#0A0A0A] placeholder-[#A3A3A3] text-white geist-regular focus:outline-none focus:border-[#27272a] transition-colors duration-500 hover:border-1 hover:border-[#27272a]'
               value={formData.userEmail}
               onChange={handleChange}
             />
@@ -83,19 +71,16 @@ const Contact: React.FC = () => {
             name='userMessage'
             required
             placeholder='Introduce your message'
-            className='contact-input p-4 rounded-lg border-2 border-[#0e0e0e] bg-[#0A0A0A] placeholder-[#A3A3A3] text-white geist-regular h-44 resize-none mb-8 focus:outline-none focus:border-[#27272a] transition-colors duration-300 hover:border-1 hover:border-[#27272a]'
+            className='contact-input p-4 rounded-lg border-2 border-[#0e0e0e] bg-[#0A0A0A] placeholder-[#A3A3A3] text-white geist-regular h-44 resize-none mb-8 focus:outline-none focus:border-[#27272a] transition-colors duration-500 hover:border-1 hover:border-[#27272a]'
             value={formData.userMessage}
             onChange={handleChange}
           />
-          <p
-            className='contact-message absolute self-center bottom-20 text-sm text-white geist-bold'
-            id='contact-message'
-          >
+          <p className='contact-message absolute self-center bottom-20 text-sm text-white geist-bold' id='contact-message'>
             {message}
           </p>
           <button
             type='submit'
-            className='button contact-button border-2 border-[#0e0e0e] bg-[#0A0A0A] hover:bg-[#121212] text-[#A3A3A3] geist-bold p-5 rounded-lg cursor-pointer self-center'
+            className='button contact-button border-2 border-[#0e0e0e] bg-[#0A0A0A] transitions-colors duration-500 hover:bg-[#121212] text-[#A3A3A3] geist-bold p-5 rounded-lg cursor-pointer self-center'
           >
             Send Message
           </button>
