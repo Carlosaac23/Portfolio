4fb701e6b649f36cf9fdb03c8957686a8dc8876a
Carlos Acosta <carlosaac23@hotmail.com>
2024-08-17 17:02:30 -0500

    Changed format images. Added meta description to index.html. Added rel to a tags

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,18 +10,18 @@ function HomePage() {
       <p className='text-left text-[#e4e4e7]'>{t('description')} 💪🏻</p>
       <div className='flex flex-col gap-5'>
         <div className='flex w-[100%] justify-between gap-5'>
-          <img src='/images/vertical-2.png' alt='' className='w-[35%] rounde-md' />
-          <img src='/images/horizontal-2.png' alt='' className='w-[65%] rounded-md' />
+          <img src='images/vertical-2.webp' alt='' className='w-[35%] rounde-md' />
+          <img src='/images/horizontal-2.webp' alt='' className='w-[65%] rounded-md' />
         </div>
         <div className='flex w-[100%] justify-between gap-5'>
-          <img src='/images/horizontal-1.png' alt='' className='w-[65%] rounded-md' />
-          <img src='/images/vertical-1.png' alt='' className='w-[35%] rounde-md' />
+          <img src='/images/horizontal-1.webp' alt='' className='w-[65%] rounded-md' />
+          <img src='/images/vertical-1.webp' alt='' className='w-[35%] rounde-md' />
         </div>
       </div>
       <p className='text-left'>{t('social')} 👇🏻</p>
 
       <div className='grid md:grid-cols-[1fr_1fr_1fr] gap-5 text-[14px]'>
-        <a href='https://github.com/Carlosaac23' target='_blank'>
+        <a href='https://github.com/Carlosaac23' target='_blank' rel='noopener noreferrer'>
           <div className='flex items-center p-3 justify-between bg-[#0a0a0a] border border-[#27272a] rounded-lg hover:border-[#52525b] transition-all duration-300 ease'>
             <img src='/images/github.svg' alt='GitHub Logo' className='w-7 h-7' />
             <div className='text-left'>
@@ -30,7 +30,7 @@ function HomePage() {
             <FaArrowUpRightFromSquare />
           </div>
         </a>
-        <a href='https://x.com/Carlosaac23' target='_blank'>
+        <a href='https://x.com/Carlosaac23' target='_blank' rel='noopener noreferrer'>
           <div className='flex items-center p-3 justify-between bg-[#0a0a0a] border border-[#27272a] rounded-lg hover:border-[#52525b] transition-all duration-300 ease'>
             <img src='/images/x.svg' alt='X Logo' className='w-7 h-7' />
             <div className='text-left'>
@@ -39,7 +39,7 @@ function HomePage() {
             <FaArrowUpRightFromSquare />
           </div>
         </a>
-        <a href='https://www.linkedin.com/in/carlos-acosta-7aa448263/' target='_blank'>
+        <a href='https://www.linkedin.com/in/carlos-acosta-7aa448263/' target='_blank' rel='noopener noreferrer'>
           <div className='flex items-center p-3 justify-between bg-[#0a0a0a] border border-[#27272a] rounded-lg hover:border-[#52525b] transition-all duration-300 ease'>
             <img src='/images/linkedin.svg' alt='Linkeding Logo' className='w-7 h-7' />
             <div className='text-left'>
