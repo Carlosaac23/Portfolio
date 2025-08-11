4e25b672db5efe0e573d4f0443c90dc40ce92b4c
Carlos <carlosaac232001@gmail.com>
2025-08-10 19:02:23 -0500

    fix(projects): show four loading cards intead of five

--- src/app/projects/loading.tsx
+++ src/app/projects/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <section className='flex flex-col gap-10 px-2'>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-5 text-sm'>
-        {Array.from({ length: 5 }, (_, index) => (
+        {Array.from({ length: 4 }, (_, index) => (
           <div key={index} className='animate-pulse bg-gray-200 dark:bg-[#333333] rounded-md p-4 h-24 sm:h-32 w-full'></div>
         ))}
       </div>
