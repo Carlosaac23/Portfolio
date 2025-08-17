a4893a96d5db26b9d07a142802a06deff69afd6e
Carlos <carlosaac232001@gmail.com>
2025-08-17 16:02:18 -0500

    feat(page): rewrite home page content with neutral palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default function HomePage() {
   return (
-    <div className='flex flex-col gap-10 text-[rgb(51,51,51)] dark:text-white'>
-      <h1 className='text-left text-2xl'>Carlos Acosta</h1>
-      <p className='text-left text-pretty text-[#333333] max-sm:leading-loose dark:text-[#ededed]'>
+    <div className='flex flex-col gap-10 text-neutral-600 dark:text-neutral-50'>
+      <h1 className='text-left text-neutral-950 dark:text-neutral-50 text-2xl'>Carlos Acosta</h1>
+      <p className='text-left text-pretty max-sm:leading-loose'>
         I am a{' '}
         <a
           href='https://zajuna.sena.edu.co/pdfs/titulada/tecgnologias/analisis_desarrollo_software.pdf'
           target='_blank'
           rel='noopener noreferrer'
-          className='font-bold text-[#171717] dark:text-white'
+          className='font-bold text-neutral-950 dark:text-neutral-50'
         >
           software analysis and development
         </a>{' '}
@@ -17,13 +17,13 @@ export default function HomePage() {
           href='https://www.sena.edu.co/es-co/Paginas/default.aspx'
           target='_blank'
           rel='noopener noreferrer'
-          className='font-bold text-[#171717] dark:text-white'
+          className='font-bold text-neutral-950 dark:text-neutral-50'
         >
           SENA,
         </a>{' '}
         and I love the tech industry. I&apos;m learning and creating projects to improve my skills and enter the tech world. I have a{' '}
-        <span className='italic font-semibold'>special taste</span> for{' '}
-        <a href='https://www.vercel.com' target='_blank' rel='noopener noreferrer' className='font-bold text-[#171717] dark:text-white'>
+        <em className='font-semibold text-neutral-950 dark:text-neutral-50'>special taste</em> for{' '}
+        <a href='https://www.vercel.com' target='_blank' rel='noopener noreferrer' className='font-bold text-neutral-950 dark:text-neutral-50'>
           Vercel
         </a>{' '}
         products, and it&apos;s a great motivation for me. I&apos;m excited to continue growing, facing new challenges, and learning more daily in this
