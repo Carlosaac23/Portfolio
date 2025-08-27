6d177973b086b1938d8b7c4a1acd5d23a3529a01
Carlos <carlosaac232001@gmail.com>
2025-08-27 17:14:29 -0500

    refactor(projectCard): add destructuring method to Project

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,21 +9,23 @@ export interface Project {
 }
 
 export default function ProjectCard({ project }: { project: Project }) {
+  const { name, repo_url, homepage_url, description, stack } = project;
+
   return (
     <div
-      key={project.name}
+      key={name}
       className='flex flex-col p-3 justify-between bg-neutral-50 border border-neutral-300 rounded-md max-h-44 hover:border-neutral-400 max-sm:w-auto shadow-xs dark:bg-neutral-950 dark:border-neutral-800 dark:hover:border-neutral-600'
     >
       <div className='text-left'>
-        <h2 className='text-neutral-950 dark:text-neutral-50 text-lg'>{project.name}</h2>
+        <h2 className='text-neutral-950 dark:text-neutral-50 text-lg'>{name}</h2>
         <div className='text-neutral-700 dark:text-neutral-200 text-sm max-h-14 overflow-y-auto'>
-          <p className='mr-1 text-left text-pretty'>{project.description || 'No description available'}</p>
+          <p className='mr-1 text-left text-pretty'>{description || 'No description available'}</p>
         </div>
         <div className='mt-2'></div>
-        {project.homepage_url ? (
+        {homepage_url ? (
           <a
             title='A live page link to this project'
-            href={project.homepage_url}
+            href={homepage_url}
             target='_blank'
             rel='noopener noreferrer'
             className='text-neutral-600 text-sm hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50 hover:underline'
@@ -39,7 +41,7 @@ export default function ProjectCard({ project }: { project: Project }) {
 
         <a
           title='A repository link to this project'
-          href={project.repo_url}
+          href={repo_url}
           target='_blank'
           rel='noopener noreferrer'
           className='text-neutral-600 text-sm hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50 hover:underline'
@@ -48,7 +50,7 @@ export default function ProjectCard({ project }: { project: Project }) {
         </a>
       </div>
       <ul className='flex flex-wrap gap-2 mt-2 py-0.5 overflow-y-auto'>
-        {project.stack.map(t => (
+        {stack.map(t => (
           <li key={t} className='px-2 py-1 bg-neutral-50 dark:bg-neutral-950 rounded-sm text-xs border border-neutral-300 shadow-xs dark:border-neutral-700'>
             {t}
           </li>
