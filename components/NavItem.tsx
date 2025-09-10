ef4416c5f07a3d340bca56df05d753c8e75af639
Carlos <carlosaac232001@gmail.com>
2025-09-09 21:12:24 -0500

    style: delete bg hover in links

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -16,7 +16,7 @@ export function NavItem({ path, title }: NavItemProps) {
     <Link
       href={{ pathname: path }}
       prefetch
-      className={`rounded-sm px-3 py-1 duration-150 ease-out hover:bg-neutral-200 hover:text-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 ${isActive ? 'text-neutral-950 dark:text-neutral-50' : ''}`}
+      className={`hover:text-neutral-95 gap-4 dark:hover:text-neutral-50 ${isActive ? 'text-neutral-950 dark:text-neutral-50' : ''}`}
     >
       {title}
     </Link>
