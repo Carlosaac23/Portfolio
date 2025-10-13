7bce601912aeeb1055c106bc3505b5612b67b1d0
Carlos <carlosaac232001@gmail.com>
2025-10-13 17:36:47 -0500

    refactor: update component Footer to something more minimalist

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,41 +2,8 @@
 
 export default function Footer() {
 	return (
-		<footer className='mt-10 flex items-center justify-center gap-6 text-neutral-500 max-sm:mt-24'>
-			<a
-				className='text-sm hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-				href='https://x.com/Carlosaac23'
-				rel='noopener noreferrer'
-				target='_blank'
-				title='A link to my GitHub'
-			>
-				Twitter/X
-			</a>
-			<a
-				className='text-sm hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-				href='https://github.com/Carlosaac23'
-				rel='noopener noreferrer'
-				target='_blank'
-				title='A link to my GitHub'
-			>
-				GitHub
-			</a>
-			<a
-				className='text-sm hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-				href='https://www.linkedin.com/in/carlos-acosta-7aa448263/'
-				rel='noopener noreferrer'
-				target='_blank'
-				title='A link to my Linkedin'
-			>
-				LinkedIn
-			</a>
-			<a
-				className='text-sm hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-				href='mailto:carlosaac232001@gmail.com'
-				title='A link to send me an email'
-			>
-				Email
-			</a>
+		<footer className='mt-10 flex items-center justify-center gap-6 text-neutral-600 text-sm'>
+			Carlos Acosta © {new Date().getFullYear()}
 		</footer>
 	);
 }
