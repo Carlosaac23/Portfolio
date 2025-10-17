d826c0ec979f9800661ed21e4346eff5f24cf452
Carlos <carlosaac232001@gmail.com>
2025-10-16 19:24:42 -0500

    refactor commit

--- app/writing/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Writing() {
-	return <h1 className='text-center'>Coming Soon</h1>;
-}
--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Footer() {
-	return (
-		<footer className='mt-10 flex items-center justify-center gap-6 text-neutral-600 text-sm'>
-			Carlos Acosta © {new Date().getFullYear()}
-		</footer>
-	);
-}
