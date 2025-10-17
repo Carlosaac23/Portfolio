edc630abdf020d7dbf1887933766b815ec18d05a
Carlos <carlosaac232001@gmail.com>
2025-10-16 19:18:54 -0500

    chore: update next config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  typedRoutes: true,
   images: {
     remotePatterns: [new URL('https://github.com/Carlosaac23.png')],
   },
