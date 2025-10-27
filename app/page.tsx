d7bf902af780abaa2c557322536c14af3fd8195a
Carlos <carlosaac232001@gmail.com>
2025-10-27 13:26:21 -0500

    refactor: import the Links section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import Links from '@/components/Links';
 import ProjectCard from '@/components/ProjectCard';
 import getProjects from '@/lib/github-services';
-import { SquareArrowOutUpRight } from 'lucide-react';
 import Balancer from 'react-wrap-balancer';
 
 export default async function HomePage() {
@@ -22,57 +22,7 @@ export default async function HomePage() {
         </Balancer>
       </p>
 
-      <div className='mb-6 flex gap-3 lg:gap-4'>
-        <a
-          className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-          href='https://x.com/Carlosaac23'
-          rel='noopener noreferrer'
-          target='_blank'
-          title='A link to my GitHub'
-        >
-          <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
-            X
-            <SquareArrowOutUpRight size={13} />
-          </span>
-        </a>
-
-        <a
-          className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-          href='https://github.com/Carlosaac23'
-          rel='noopener noreferrer'
-          target='_blank'
-          title='A link to my GitHub'
-        >
-          <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
-            GitHub
-            <SquareArrowOutUpRight size={13} />
-          </span>
-        </a>
-
-        <a
-          className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-          href='https://www.linkedin.com/in/carlos-acosta-7aa448263/'
-          rel='noopener noreferrer'
-          target='_blank'
-          title='A link to my Linkedin'
-        >
-          <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
-            LinkedIn
-            <SquareArrowOutUpRight size={13} />
-          </span>
-        </a>
-
-        <a
-          className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
-          href='mailto:carlosaac232001@gmail.com'
-          title='A link to send me an email'
-        >
-          <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
-            Email
-            <SquareArrowOutUpRight size={13} />
-          </span>
-        </a>
-      </div>
+      <Links />
 
       <section className='flex flex-col gap-6 text-neutral-950 dark:text-neutral-50'>
         {projects.length > 0 ? (
