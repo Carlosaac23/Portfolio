4af29c2ea0c4b18c4fd585980425f3125c100bb0
Carlos <carlosaac232001@gmail.com>
2025-10-27 13:26:05 -0500

    feat: create Links components to maintain the main page clean

---
+++ components/Links.tsx
@@ -0,0 +1,57 @@
+import { SquareArrowOutUpRight } from 'lucide-react';
+
+export default function Links() {
+  return (
+    <div className='mb-6 flex gap-3 lg:gap-4'>
+      <a
+        className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
+        href='https://x.com/Carlosaac23'
+        rel='noopener noreferrer'
+        target='_blank'
+        title='A link to my GitHub'
+      >
+        <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
+          X
+          <SquareArrowOutUpRight size={13} />
+        </span>
+      </a>
+
+      <a
+        className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
+        href='https://github.com/Carlosaac23'
+        rel='noopener noreferrer'
+        target='_blank'
+        title='A link to my GitHub'
+      >
+        <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
+          GitHub
+          <SquareArrowOutUpRight size={13} />
+        </span>
+      </a>
+
+      <a
+        className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
+        href='https://www.linkedin.com/in/carlos-acosta-7aa448263/'
+        rel='noopener noreferrer'
+        target='_blank'
+        title='A link to my Linkedin'
+      >
+        <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
+          LinkedIn
+          <SquareArrowOutUpRight size={13} />
+        </span>
+      </a>
+
+      <a
+        className='text-sm text-neutral-600 hover:text-neutral-950 dark:text-neutral-400 dark:hover:text-neutral-50'
+        href='mailto:carlosaac232001@gmail.com'
+        title='A link to send me an email'
+      >
+        <span className='inline-flex items-center gap-0.5 hover:underline hover:underline-offset-2'>
+          Write me
+          <SquareArrowOutUpRight size={13} />
+        </span>
+      </a>
+    </div>
+  );
+}
