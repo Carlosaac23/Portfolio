2c03eb4b5779dad97119faad12408320d73774ba
Carlos <carlosaac232001@gmail.com>
2026-01-17 11:45:49 -0500

    refactor: use simple function to get actual year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,7 @@
-async function getCurrentYear() {
-  return new Date().getFullYear();
-}
-
 export default async function Footer() {
   return (
     <footer className='mt-10 flex items-center justify-center gap-6 text-sm text-neutral-500 dark:text-neutral-400'>
-      Carlos Acosta © {await getCurrentYear()}
+      Carlos Acosta © {new Date().getFullYear()}
     </footer>
   );
 }
