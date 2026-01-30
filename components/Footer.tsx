b335dbd7e06cf169823f7e03492e32ae6262358b
Carlos <carlosaac232001@gmail.com>
2026-01-30 09:23:58 -0500

    fix: add more margin-top to prevent crash in mobile screens

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='mt-auto'>
+    <footer className='mt-6'>
       <p className='text-center text-sm text-neutral-500 dark:text-neutral-400'>
         Carlos Acosta © {new Date().getFullYear()}
       </p>
